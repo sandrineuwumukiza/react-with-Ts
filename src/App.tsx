2d1d63466895960c2afc9977727039645127ce69
sandrineuwumukiza <uwasandrine99@gmail.com>
2024-04-21 00:51:03 +0200

    react js with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/greet';
+import { Person } from './components/person';
+import { PersonList } from './components/personList';
+import { Status } from './components/status';
+import { Heading } from './components/heading';
+import { Oscar } from './components/oscar';
 
 function App() {
+  const PersonName = {
+    first:'Bruce',
+    last:'Wayne  '
+  }
+
+  const nameList = [{
+    first:'Bruce',
+    last:'Wayne'
+  },
+  {
+    first:'Clark',
+    last:'Kent'
+  },
+  {
+    first:'Princess',
+    last:'Diana'
+  },
+]
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name = 'Vishwas' isLoggedIn={false}/>
+      <Person name={PersonName}/>
+      <PersonList names={nameList}/>
+      <Status status='loading'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario</Heading>
+      </Oscar>
+      
     </div>
   );
 }
